a591210fe3ad31aa065f00dabfe257c4352da43b
ThanasisKl <tklettas@gmail.com>
2021-11-27 22:15:16 +0200

    report

--- src/NqueensProblem/Main.java
+++ src/NqueensProblem/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Genetic g = new Genetic();
         long start = System.currentTimeMillis();  //calculates time
                          //runs genetic algorithm (population size, mutation probabiity, max-loops, N)
-        Chromosome chromosome = g.geneticAlgorithm(300,0.08,100000,8); //runs genetic algorithm (population size)
+        Chromosome chromosome = g.geneticAlgorithm(300,0.08,10000,8); //runs genetic algorithm (population size)
         long end = System.currentTimeMillis();
         chromosome.showChessboard();
         System.out.println("\nTotal time of searching: " + (double)(end - start) / 1000 + " sec."); //prints time
